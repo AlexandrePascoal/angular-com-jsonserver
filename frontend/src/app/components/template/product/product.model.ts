e7185f5617727fb69f8b3ec182711d6f91c3176c
Alexandre Pascoal <alexandre_medeiros11@hotmail.com>
2020-05-16 00:21:24 -0300

    Implementacao do arquivo de modelo do produto para o back-end

---
+++ frontend/src/app/components/template/product/product.model.ts
@@ -0,0 +1,11 @@
+export interface Produto {
+    /*
+    - Arquivo de modelo para o back-end
+    - Tem que estar com os mesmos atributos declarados
+    - O id está como atributo Opcional
+    */
+
+    id?: number
+    name: string
+    price: number
+}
